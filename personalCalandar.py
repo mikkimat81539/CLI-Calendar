c08df93a0ee28e583be78009b791cf8e679fa29f
mikkimat81539 <mmatthews81539@gmail.com>
2026-05-02 02:47:07 +0000

    added conditions for months

--- personalCalandar.py
+++ personalCalandar.py
@@ -1,16 +1,30 @@
-import re
+import re, string
 
 def main():
 	# USER INPUT
-	pickMonth = input("Enter a month: ")
-
 	pickYear = input("Enter a year: ")
 
 	# LEAP YEAR FORMULA
 	leapYr = int(pickYear) % 4
 
+	# DAYS
+	leapDays = 366
+	regDays = 365
+
+	# MONTHS
+	months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+
 	if len(pickYear) != 4:
+		print("Invalid Input\n")
 		main()			
+
+	pickMonth = input("\nEnter a month: ").upper()
+
+	for i in months:
+		same = re.match(i, pickMonth)
+		if same:
+			print(i)
+			break
 
 	else:
 		print(pickMonth, pickYear)
